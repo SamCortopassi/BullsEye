7a3be80472f7b628aa88431731c74cd846116eb4
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-01-18 18:18:39 -0600

    new views

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Samantha Cortopassi on 1/18/19.
+//  Copyright © 2019 Samantha Cortopassi. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func goBack(_ sender: Any) {
+        
+    }
+    
+    
+
+}
